4d5692420329a12685fdc6cacd9ea16d99d62ff1
sakpaldeepti <95605003+sakpaldeepti@users.noreply.github.com>
2024-04-07 23:43:13 +0530

    Initial commit

--- app/src/main/java/com/example/stacklabproject/model/Home.kt
+++ app/src/main/java/com/example/stacklabproject/model/Home.kt
@@ -1,40 +1,46 @@
-package com.example.stacklabproject.model
-
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.stacklabproject.R
 import com.example.stacklabproject.ui.cart.CartScreen
 import com.example.stacklabproject.ui.home.HomeScreen
 
-class Home : AppCompatActivity() {
-
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val composeLayout = findViewById<ComposeView>(R.id.compose_view)
-        composeLayout.setContent {
-                val navController = rememberNavController()
-                NavHost(navController = navController, startDestination = "mainScreen") {
-                    composable("mainScreen") {
-                        HomeScreen(navController)
-                    }
-                    composable("cartScreen") {
-                        CartScreen(navController)
-                    }
+        setContent {
+            MyApp()
+        }
+    }
+}
+
+@Composable
+fun MyApp() {
+    val navController = rememberNavController()
+    MaterialTheme {
+        Surface {
+            NavHost(navController = navController, startDestination = "home") {
+                composable("home") {
+                    HomeScreen(navController)
                 }
+                composable("cart") {
+                    CartScreen(navController)
+                }
+            }
         }
     }
 }
 
 @Preview
 @Composable
-fun PreviewNewScreenComposable() {
-        HomeScreen(navController = rememberNavController())
-
+fun PreviewMainActivity() {
+    MyApp()
 }
+
+
